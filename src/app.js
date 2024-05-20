c5452476ce9365fa98efb4ff08d43fffaf628ee3
Erwin Darsono <erwindarsono08@gmail.com>
2024-05-20 13:06:40 +0800

    server initialization

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    })
+})
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
