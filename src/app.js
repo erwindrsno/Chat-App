33271a6f6b461d427e9ce1b31e182ada1330dc87
Erwin Darsono <erwindarsono08@gmail.com>
2024-05-21 17:27:54 +0800

    parsing data OK

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { WebSocketServer } from 'ws';
+import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
@@ -8,15 +9,20 @@ const wss = new WebSocketServer( { server, path: '/web-socket' } );
 
 const PORT = 3000;
 
-app.use(express.static('public'));
+const staticPath = path.resolve('public');
+app.use(express.static(staticPath));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 wss.on('connection', (socket) => {
     console.log(`a user connected`);
 
     socket.send("Welcome to chat app! -server");
 
-    socket.on('message', message => {
-        console.log(`the message is ${message}`);
+    socket.on('message', data => {
+        const pesan = JSON.parse(data);
+        console.log(pesan.chat_message);
     })
 
     socket.on('close', () => {
