cf20c7359d55e907fb6aa8a8cec05787c2a68453
Erwin Darsono <erwindarsono08@gmail.com>
2024-05-26 20:54:40 +0800

    rename html file

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,10 @@
-const greetings = document.getElementById('greetings');
+// const greetings = document.getElementById('greetings');
 
 // greetings.addEventListener('htmx:beforeRequest', event => {
 //     console.log(event.detail);
 // })
 
-const form = document.getElementById('form');
+// const form = document.getElementById('form');
 
 // form.addEventListener('htmx:wsConfigSend', event => {
 //     console.log(event.detail.parameters.chat_message);
